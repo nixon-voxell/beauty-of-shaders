194c8fa78c73d1610e604a4994baebafb54eb3cd
Nixon <voxell.technologies@gmail.com>
2023-05-10 18:29:52 +0800

    minor changes

--- src/scenes/intro.tsx
+++ src/scenes/intro.tsx
@@ -1,3 +1,5 @@
+import {COLOR} from "../styles"
+
 import {makeScene2D} from "@motion-canvas/2d/lib/scenes";
 import {Rect, Txt} from "@motion-canvas/2d/lib/components";
 import {beginSlide, createRef} from "@motion-canvas/core/lib/utils";
@@ -5,8 +7,6 @@ import {all} from "@motion-canvas/core/lib/flow";
 import {easeInOutCubic, easeInOutQuart, easeInOutQuint} from "@motion-canvas/core/lib/tweening";
 import {createSignal} from "@motion-canvas/core/lib/signals";
 import { Color, Vector2 } from "@motion-canvas/core/lib/types";
-
-import {COLOR} from "../styles"
 
 export default makeScene2D(function* (view) {
   view.fontFamily(`"Consolas", monospace`).fontWeight(700).fontSize(256);
