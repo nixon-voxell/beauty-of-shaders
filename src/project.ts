7bfb2f9608cc2d310b6774a721b820306788679c
Nixon <voxell.technologies@gmail.com>
2023-05-10 18:32:55 +0800

    init coordinate_systems scene

--- src/project.ts
+++ src/project.ts
@@ -5,6 +5,7 @@ import outline from "./scenes/outline?scene"
 import types_of_shaders from "./scenes/types_of_shaders?scene"
 import what_is_a_shader from "./scenes/what_is_a_shader?scene";
 import why_parallelism from "./scenes/why_parallelism?scene"
+import coordinate_systems from "./scenes/coordinate_systems?scene"
 
 export default makeProject({
   scenes: [
@@ -13,5 +14,6 @@ export default makeProject({
     what_is_a_shader,
     types_of_shaders,
     why_parallelism,
+    coordinate_systems,
   ],
 });
