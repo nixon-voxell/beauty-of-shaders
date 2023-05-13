aed6171cec5f1f86a97b77b3285f5b8713795c29
Nixon <voxell.technologies@gmail.com>
2023-05-13 11:29:21 +0800

    minor changes

--- src/scenes/coordinate_systems.tsx
+++ src/scenes/coordinate_systems.tsx
@@ -1,14 +1,14 @@
 import { COLOR } from "../styles"
 import {
   ContentRectConfig, ContentRect, createContentRect,
-  scaleContentRect, fadeContentRect, positionContentRect, changeContentTxt, moveContentRect
+  scaleContentRect, fadeContentRect, moveContentRect
 } from "../utils/rect_util";
 import { createTitleCont, changeTitleAtCenter, moveTitleToTopLeft } from "../utils/subtopic_util";
 
 import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
 import { Circle, Grid, Line, Rect, Txt } from "@motion-canvas/2d/lib/components";
 import { beginSlide } from "@motion-canvas/core/lib/utils";
-import { all, chain, delay, sequence, waitFor } from "@motion-canvas/core/lib/flow";
+import { all, chain, sequence, waitFor } from "@motion-canvas/core/lib/flow";
 import { easeInOutCubic, easeInOutQuart, tween} from "@motion-canvas/core/lib/tweening";
 import { Vector2 } from "@motion-canvas/core/lib/types";
 
