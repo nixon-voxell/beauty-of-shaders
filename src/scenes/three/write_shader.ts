4565423e5e4bc6b38bce529db2a8a1ecfee69a67
Nixon <voxell.technologies@gmail.com>
2023-05-21 18:02:56 +0800

    use THREE. instead of importing to avoid confusion

--- src/scenes/three/write_shader.ts
+++ src/scenes/three/write_shader.ts
@@ -1,18 +1,17 @@
 import * as THREE from "three";
 import writeShaderFragment from "./shaders/write_shader.fragment.glsl?raw";
 import networkVertex from "./shaders/network.vertex.glsl?raw";
-import { Vector2, Vector3 } from "three";
 
 const planeMaterial = new THREE.ShaderMaterial({
   uniforms: {
     utime: { value: 0.0 },
     opacity: { value: 1.0 },
-    color: { value: new Vector3(1.0, 1.0, 1.0) },
+    color: { value: new THREE.Vector3(1.0, 1.0, 1.0) },
     colorOpacity: { value: 0.0 },
     sinTimeOpacity: { value: 0.0 },
     scaledSinTimeOpacity: { value: 0.0 },
     mixTimeOpacity: { value: 0.0 },
-    shiftCoord: { value: new Vector2(0.5, 0.5) },
+    shiftCoord: { value: new THREE.Vector2(0.5, 0.5) },
     shiftedCoordOpacity: { value: 0.0 },
     uniformCoordOpacity: { value: 0.0 },
     uniformCoordLenOpacity: { value: 0.0 },
@@ -44,12 +43,12 @@ function setupScene() {
   const uniforms = planeMesh.material.uniforms;
   uniforms.utime.value = 0.0;
   uniforms.opacity.value = 1.0;
-  uniforms.color.value = new Vector3(1.0, 1.0, 1.0);
+  uniforms.color.value = new THREE.Vector3(1.0, 1.0, 1.0);
   uniforms.colorOpacity.value = 0.0;
   uniforms.sinTimeOpacity.value = 0.0;
   uniforms.scaledSinTimeOpacity.value = 0.0;
   uniforms.mixTimeOpacity.value = 0.0;
-  uniforms.shiftCoord.value = new Vector2(0.5, 0.5);
+  uniforms.shiftCoord.value = new THREE.Vector2(0.5, 0.5);
   uniforms.shiftedCoordOpacity.value = 0.0;
   uniforms.uniformCoordOpacity.value = 0.0;
   uniforms.uniformCoordLenOpacity.value = 0.0;
